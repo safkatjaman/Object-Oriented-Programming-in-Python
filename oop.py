fad8bfc9515da5777ee37f2aadb04d193ef9fbae
safkatjaman <safkatjamansiam20162@gmail.com>
2021-01-13 22:26:52 +0600

    Advanced class, inheritance, encapsulation, polymorphism

--- oop.py
+++ oop.py
@@ -113,3 +113,169 @@ myMarks.subjectMarks(35)
 
 print('Subject Name:', myMarks.subjetName)
 print('Marks Obtained:', myMarks.oldMarks)
+print('\n\n')
+
+
+
+                                                                                        #''' Advanced Class '''#
+''' There is a problem in the below code. The second customer didn't add any T-shirt in his/her cart but still it's showing T-shit and Shoes in his/her cart. This thing is called class level attribute. These attributes are shared among all the objects created from the class '''
+
+class Shopping:
+      cart = []
+      def add_to_cart(self, item):
+            self.cart.append(item)
+
+customer1 = Shopping()
+customer1.add_to_cart('T-shirt')
+print(customer1.cart)
+
+customer2 = Shopping()
+customer2.add_to_cart('Shoes')
+print(customer2.cart)
+print('\n\n')
+
+''' To solve this problem we have to make a constructor in the class and create the cart attribute inside the constructor. Then, every object from this class will have a separate cart attribute. This is called an instance attribute. '''
+
+class Shopping2:
+      def __init__(self):
+            self.cart = []
+
+      def add_to_cart_2(self, item):
+            self.cart.append(item)
+
+customer01 = Shopping2()
+customer01.add_to_cart_2('T-shirt')
+print(customer01.cart)
+
+customer02 = Shopping2()
+customer02.add_to_cart_2('Shoes')
+print(customer02.cart)
+print('\n\n')
+
+
+
+                                                                                       #''' Banking Software '''#
+''' Class methods are functions you can add if-else, loop or any kind of calculation inside it. '''
+
+class Bank:
+      def __init__(self):
+            self.balance = 10000
+            self.minimum_balance = 1000
+      
+      def get_balance(self):
+            return self.balance
+      
+      def withdraw(self, amount):
+            if amount < self.minimum_balance:
+                  print('Sorry! You can\'t withdraw less than 1000 BDT')
+                  print('Account balance:', self.balance, 'BDT')
+
+            elif amount > self.balance:
+                  print('Sorry Sir/Mam! You have', self.balance, 'BDT.', 'You can\'t withdraw more than that from your account.')
+
+            else:
+                  print('Previous Balance:', self.balance, 'BDT')
+                  print('Withdraw Amount:', amount, 'BDT')
+                  self.balance = self.balance - amount
+                  print('Current Balance:', self.balance, 'BDT')
+                  return amount
+
+myAccount = Bank()
+
+myAccount.withdraw(20000)
+print('\n\n')
+
+
+                                                                                        #''' Inheritance '''#
+''' Inheritance is easy to understand. Just think that Bill Gates is your father. If he dies you will get all of his wealth by inheritance. Another example is like, you are running a store where you sell smart phones, smart watches and tablets. '''
+''' Smartphones: 1) brand name 2) price 3) video call 4) recharge battery ''' 
+''' Smartwatches: 1) brand name 2) price 3) count walking steps 4) heart rate '''
+''' tanlets: 1) brand name 2) price 3) can read books 4) play stupic games '''
+''' These three smart devices have some attributes in common, like their brand name and price. '''
+
+''' In the below code we created a class named SmartDevice because both the Phone2 and Watch class had a same method named recharge(). So we created the recharge method in the SmartDevice class and passed the SmartDevice through the parameter in the Phone class and Watch class. '''
+
+''' The class you inherit from is called Parent Class and the class that is inheritting is called Child Class. Here SmartDevice is the parent class and Phone2 & Watch are the child classes. '''
+
+class SmartDevice():
+      def __init__(self, brand, price):
+            self.brand = brand
+            self.price = price
+      def recharge(self):
+            print('Eating electrons')
+            print('Electrons are yummy!')
+
+class Phone2(SmartDevice):
+      video_call = True
+      def __init__(self, brand, price, network):
+            SmartDevice.__init__(self, brand, price)
+            self.network = network
+
+myPhone2 = Phone2('Samsung', 30000, 'Teletalk')
+myPhone2.recharge()
+
+print('\n')
+
+class Watch(SmartDevice):
+      def __init__(self, brand, price, has_gps):
+            SmartDevice.__init__(self, brand, price)
+            self.has_gps = has_gps
+
+myWatch = Watch('Samsung', 20000, True)
+myWatch.recharge()
+print('\n\n')
+
+
+
+                                                                                    #''' Encapsulation '''#
+''' While coding you might want to protect some sensitive information from the user so that outsiders can't access or modify your private information. The act of protecting or encapsulating something is called Encapsulation '''
+
+''' While creating a class if you create an attribute which starts with double underscores __ then the attribute will be private and no one will be able to access it from outside. '''
+
+''' Though we can't access the attribute outside of the class but we can do it inside. Like below code, we are using the get_balance method to access the __balance attribute and showing it through the get_balance method '''
+
+class Banking:
+      def __init__(self):
+            self.name = ''
+            self.__balance = 1000
+      def get_balance(self):
+            return self.__balance
+      
+account = Banking()
+account.name = 'Safkat Jaman'
+balance = account.get_balance()
+print(account.name, balance)
+print('\n\n')
+
+
+
+                                                                                    #''' Polymorphism '''#
+''' When the code can take many forms. it's called polymorphism. The word poly means many and morph means a change of shape. Here the move method can take many forms. You are calling the move method. based on the class it's on, it calls the right method in the right class. '''
+
+class Car:
+      car_name = 'Mercedes'
+
+      def move(self):
+            print(self.car_name, 'is moving')
+
+class Truck:
+      truck_name = 'Cyber Truck'
+
+      def move(self):
+            print(self.truck_name, 'is moving')
+
+class Bike:
+      bike_name = 'R15'
+
+      def move(self):
+            print(self.bike_name, 'is moving')
+
+vehicles = [Car(), Truck(), Bike()]
+for moving in vehicles:
+      moving.move()
+
+
+
+                                                                                          #''' Summary '''#
+''' Use Inheritance to reduce repetition of attributes and methods. To inherit, just pass the parent class to the child class. '''
+''' Encapsulation is the process of protecting attributes from outsiders. To create private attributes you will need to use two underscores before the name of the attribute like this, __balance. '''
